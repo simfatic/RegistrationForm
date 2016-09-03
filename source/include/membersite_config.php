31054243fc19741f52c56a0ea9839109ad974d1e
qraeciella <priscilla.gorisson@gmail.com>
2016-09-04 01:31:20 +0200

    Update membersite_config.php

--- source/include/membersite_config.php
+++ source/include/membersite_config.php
@@ -4,23 +4,23 @@ require_once("./include/fg_membersite.php");
 $fgmembersite = new FGMembersite();
 
 //Provide your site name here
-$fgmembersite->SetWebsiteName('user11.com');
+$fgmembersite->SetWebsiteName('clcdenhaag.nl');
 
 //Provide the email address where you want to get notifications
-$fgmembersite->SetAdminEmail('user11@user11.com');
+$fgmembersite->SetAdminEmail('priscilla.gorisson@gmail.com');
 
 //Provide your database login details here:
 //hostname, user name, password, database name and table name
 //note that the script will create the table (for example, fgusers in this case)
 //by itself on submitting register.php for the first time
-$fgmembersite->InitDB(/*hostname*/'localhost',
-                      /*username*/'prasanth',
-                      /*password*/'p',
-                      /*database name*/'testdb',
-                      /*table name*/'fgusers3');
+$fgmembersite->InitDB(/*hostname*/'CLC',
+                      /*username*/'qraeciella',
+                      /*password*/'Priscilla93',
+                      /*database name*/'Inschrijfformulieren events',
+                      /*table name*/'gegevens');
 
 //For better security. Get a random string from this link: http://tinyurl.com/randstr
 // and put it here
-$fgmembersite->SetRandomKey('qSRcVS6DrTzrPvr');
+$fgmembersite->SetRandomKey('pYzrVzw6iBuaCc1');
 
 ?>
