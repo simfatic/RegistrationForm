20965134e49c0833fe21f1aa64d9387b135b1b5b
root <root@localhost.localdomain>
2019-10-06 00:24:24 -0400

    Added comments

--- source/login.php
+++ source/login.php
@@ -33,12 +33,14 @@ if(isset($_POST['submitted']))
 <div><span class='error'><?php echo $fgmembersite->GetErrorMessage(); ?></span></div>
 <div class='container'>
     <label for='username' >UserName*:</label><br/>
-    <input type='text' name='username' id='username' value='<?php echo $fgmembersite->SafeDisplay('username') ?>' maxlength="50" /><br/>
+    <input type='text' name='username' id='username' value='<?php echo $fgmembersite->SafeDisplay('username') ?>' maxlength="50" /> //username input
+<br/>
     <span id='login_username_errorloc' class='error'></span>
 </div>
 <div class='container'>
     <label for='password' >Password*:</label><br/>
-    <input type='password' name='password' id='password' maxlength="50" /><br/>
+    <input type='password' name='password' id='password' maxlength="50" /> //password input
+<br/>
     <span id='login_password_errorloc' class='error'></span>
 </div>
 
